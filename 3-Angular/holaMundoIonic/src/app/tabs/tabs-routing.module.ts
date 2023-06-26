64906cc7735603871998f16a4252ac7040e3f372
zamelkev <kevin.zamora.amela@gmail.com>
2023-06-26 14:23:47 +0200

    in progress & update / Class Code 26-6-23

--- 3-Angular/holaMundoIonic/src/app/tabs/tabs-routing.module.ts
+++ 3-Angular/holaMundoIonic/src/app/tabs/tabs-routing.module.ts
@@ -20,6 +20,10 @@ const routes: Routes = [
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
+        path: 'tab4',
+        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+      },
+      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full'
@@ -28,7 +32,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/tab2',
     pathMatch: 'full'
   }
 ];
