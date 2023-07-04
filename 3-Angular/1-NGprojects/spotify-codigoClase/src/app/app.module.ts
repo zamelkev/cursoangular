c01ca87b36f9f921a8e189f9fb3003e6f06dd723
zamelkev <kevin.zamora.amela@gmail.com>
2023-07-04 13:01:40 +0200

    update / Spotify Exercise Files Update

--- 3-Angular/1-NGprojects/spotify-codigoClase/src/app/app.module.ts
+++ 3-Angular/1-NGprojects/spotify-codigoClase/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GeneralesModule } from './generales/generales.module';
 import { VistasModule } from './vistas/vistas.module';
 
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    RouterModule,
     BrowserModule,
     AppRoutingModule,
     GeneralesModule,
