d312da0e14c04f4b53b7e7cbebdfead86729f5da
Kevin Zamora <kevin.zamora.amela@gmail.com>
2023-07-15 14:07:26 +0200

    feature / Add Module's Importations into app.module.ts

--- 3-Angular/3-KZprojects/KevinZamora-CV/src/app/app.module.ts
+++ 3-Angular/3-KZprojects/KevinZamora-CV/src/app/app.module.ts
@@ -4,11 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AnotherInformationModule } from './another-information/another-information.module';
+import { EducationalExperienciesModule } from './educational-experiencies/educational-experiencies.module';
+import { MainComponentsModule } from './main-components/main-components.module';
+import { PersonalDataModule } from './personal-data/personal-data.module';
+import { VolunteeringAndSocialActionsModule } from './volunteering-and-social-actions/volunteering-and-social-actions.module';
+import { WorkingExperiencesModule } from './working-experiences/working-experiences.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AnotherInformationModule,
+    EducationalExperienciesModule,
+    MainComponentsModule,
+    PersonalDataModule,
+    VolunteeringAndSocialActionsModule,
+    WorkingExperiencesModule,
     BrowserModule,
     AppRoutingModule
   ],
